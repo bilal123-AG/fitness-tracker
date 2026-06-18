87e8444c34ccd713492902fc2871b88f88b0872a
bilal <bilal123@qq.com>
2026-06-18 17:13:14 +0800

    添加部署脚本

---
+++ 部署.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+chcp 65001 >nul
+cd /d E:\健身小程序
+
+echo 🔗 正在推送至 Gitee...
+git push -u origin master
+
+echo.
+echo ✅ 推送完成！
+echo 📌 接下来：Gitee 仓库 -> 服务 -> Gitee Pages -> 部署
+echo 🎉 稍等片刻即可上线！
+pause
